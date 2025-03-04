da9a87b1c408fa2a5f385cb5c7eb5310b2413b6f
Dan Hunt <dkhunt27@gmail.com>
2025-03-04 15:32:24 -0500

    clean up

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,10 +3,6 @@ import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import { PageHeader } from '../components/pageHeader/page-header';
 import './dashboard.css';
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
-TimeAgo.addDefaultLocale(en);
-TimeAgo.addLocale(en);
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
